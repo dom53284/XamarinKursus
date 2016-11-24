494363127cb3201ba5da0d7b33bb2109ff15a527
John Doe <fls@sidor.dk>
2016-11-24 14:38:24 +0100

    Update 1

---
+++ MiniExtensionOpgave/MiniExtensionOpgave/MiniExtensionOpgave/MyMarkupExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+
+namespace MiniExtensionOpgave
+{
+    class MyMarkupExtension : IMarkupExtension
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return "Hvor mange æbler vil jeg have? " + (A + B);
+        }
+    }
+}
