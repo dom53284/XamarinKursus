c799505ac1d310e6fb49608b8b74fca3b7c86c01
John Doe <fls@sidor.dk>
2016-11-25 13:47:46 +0100

    OneShotDateTimePage + ClockPage

---
+++ OneShotDateTimePage/OneShotDateTimePage/OneShotDateTimePage/App.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace OneShotDateTimePage
+{
+
+    class ClockViewModel : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ClockViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public DateTime DateTime
+        {
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this,
+                            new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+            get
+            {
+                return dateTime;
+            }
+        }
+    }
+
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new OneShotDateTimePage.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
