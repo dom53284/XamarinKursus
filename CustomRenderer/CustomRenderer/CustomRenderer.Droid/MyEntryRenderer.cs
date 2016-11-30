b8f1627380b7d18ea06c0d0b73551eaf0271a23a
John Doe <fls@sidor.dk>
2016-11-30 11:13:43 +0100

    CustomRenderer

---
+++ CustomRenderer/CustomRenderer/CustomRenderer.Droid/MyEntryRenderer.cs
@@ -0,0 +1,26 @@
+﻿using CustomRenderer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer (typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace CustomRenderer
+{
+    class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged (ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+            }
+        }
+
+    }
+}
---
+++ CustomRenderer/CustomRenderer/CustomRenderer/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustomRenderer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    new Label
+                    {
+                        Text = "Hello, Custom Renderer",
+                    },
+                    new MyEntry
+                    {
+                        Text = "In Shared Code",
+                    }
+
+                },
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+        }
+    }
+}
