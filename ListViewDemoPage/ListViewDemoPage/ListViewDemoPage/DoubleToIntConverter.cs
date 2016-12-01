d0afd3c1b1afe24604a3c0b21fd0c0df15ccdf01
John Doe <fls@sidor.dk>
2016-12-01 13:53:56 +0100

    Updates

---
+++ ListViewDemoPage/ListViewDemoPage/ListViewDemoPage/DoubleToIntConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewDemoPage
+{
+    class DoubleToIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+                              object parameter, CultureInfo culture)
+        {
+            double multiplier;
+
+            if (!Double.TryParse(parameter as string, out multiplier))
+                multiplier = 1;
+
+            return (int)Math.Round(multiplier * (double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            double divider;
+
+            if (!Double.TryParse(parameter as string, out divider))
+                divider = 1;
+
+            return ((double)(int)value) / divider;
+        }
+    
+    }
+}
